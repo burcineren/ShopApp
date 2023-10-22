50f3dacd1c52f26a01f24cab594b5656e15cb263
burcineren <burcin.ern969@gmail.com>
2023-10-22 21:18:39 +0300

    feat: auth login

--- src/app/admin/auth/auth.component.ts
+++ src/app/admin/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
-
-  constructor() { }
+  username: string;
+  password: string;
+  errorMessage:string;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  login(form: NgForm) {
+    if(form.valid){
+      if(this.username==='admin' && this.password==='123'){
+        this.router.navigateByUrl('/admin/main');
+      }
+      else{
+        this.errorMessage = 'Hatalı kullanıcı adı ve şifre girdiniz.';
+      }
+    } else {
+      this.errorMessage ='Bilgileri eksiksiz giriniz.';
+    }
+  }
 }
