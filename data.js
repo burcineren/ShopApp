817ac4e58df3fd1f7b982c5ccff1842dfd7ab138
burcineren <burcin.ern969@gmail.com>
2024-02-10 14:57:45 +0300

    data image

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 module.exports = function(){
     return {
         products: [
-            {id: 1, name: 'Samsung S5',price:1000, imageUrl: '1.jpg', description: 'test', category: 'Telefon'},
-            {id: 2, name: 'Samsung S4',price:1000, imageUrl: '2.jpg', description: 'test2', category: 'Telefon'},
-            {id: 3, name: 'Samsung S1',price:1000, imageUrl: '3.jpg', description: 'test3', category: 'Bilgisayar'},
-            {id: 4, name: 'Samsung S6',price:1000, imageUrl: '4.jpg', description: 'test4', category: 'Elektronik'},
+            {id: 1, name: 'Phone',price:1000, imageUrl: 'phone.jpg', description: 'Telefon', category: 'Telefon'},
+            {id: 2, name: 'Phone',price:1000, imageUrl: 'phone2.jpg', description: 'test2', category: 'Telefon'},
+            {id: 3, name: 'Macbook',price:1000, imageUrl: 'macbook.jpeg', description: 'test3', category: 'Bilgisayar'},
+            {id: 4, name: 'Samsung',price:1000, imageUrl: 'b-0352.jpg', description: 'test4', category: 'Elektronik'},
         ],
         categories: [
             {
